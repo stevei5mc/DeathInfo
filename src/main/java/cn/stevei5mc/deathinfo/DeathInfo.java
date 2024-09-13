56fa9132f9c1d2f6cbe6526f1cddf84b4cb245b1
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2024-09-13 22:25:35 +0800

    修正一出错误

--- src/main/java/cn/stevei5mc/deathinfo/DeathInfo.java
+++ src/main/java/cn/stevei5mc/deathinfo/DeathInfo.java
@@ -17,7 +17,7 @@ public class DeathInfo extends PluginBase {
     public void onEnable() {
         Server.getInstance().getScheduler().scheduleDelayedTask(this, () -> {
             this.getLogger().warning("§c警告! §c本插件为免费且开源的一款插件，如果你是付费获取到的那么你就被骗了");
-            this.getLogger().info("§a开源链接和使用方法: §bhttps://github.com/stevei5mc/NewTipsVariables");
+            this.getLogger().info("§a开源链接和使用方法: §bhttps://github.com/stevei5mc/DeathInfo");
         },20);
     }
 
